86add22d092244dc1049d88da8dbb3824ba90dd1
Alvin <ailustrisimo@yondu.com>
2022-11-28 15:40:38 +0800

    add: table

---
+++ src/components/Forms/Tables.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="relative">
+        <v-table>
+            <thead>
+            <tr>
+                <th class="text-left" 
+                v-for="item in headers"
+                :key="item">
+                {{ item }}
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                v-for="item in datas"
+                :key="item.name"
+            >
+                <td>{{ item.name }}</td>
+                <td>{{ item.description }}</td>
+            </tr>
+            </tbody>
+        </v-table>
+        <v-pagination
+        v-model="page"
+        :length="6"
+        ></v-pagination>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data () {
+      return {
+        page: 1,
+        pageSize: 0,
+        headers: null,
+        datas: null
+      }
+    },
+    // mounted () {
+    //     if (this.datas) {
+    //         this.pageSize = this.datas.length/5
+    //     }
+    // }
+  }
+  </script>
+  
+  <style>
+  </style>
--- src/views/EmployeeView.vue
+++ src/views/EmployeeView.vue
@@ -5,19 +5,68 @@
     <v-row class="px-2 px-md-10 pt-5 pt-md-10">
             
         <v-col  cols="12" md="6">
-            <SampleForm />
+            <tables ref="Tables"/>
         </v-col>
     </v-row>
 </template>
 
 <script>
-import SampleForm from '../components/Home/SampleForm.vue'
+import Tables from '../components/Forms/Tables.vue'
 
 export default {
-  components: { SampleForm },
+  components: { Tables },
   data() {
     return {
+      data: [
+          {
+            name: 'Frozen Yogurt',
+            description: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            description: 237,
+          },
+          {
+            name: 'Eclair',
+            description: 262,
+          },
+          {
+            name: 'Cupcake',
+            description: 305,
+          },
+          {
+            name: 'Gingerbread',
+            description: 356,
+          },
+          {
+            name: 'Jelly bean',
+            description: 375,
+          },
+          {
+            name: 'Lollipop',
+            description: 392,
+          },
+          {
+            name: 'Honeycomb',
+            description: 408,
+          },
+          {
+            name: 'Donut',
+            description: 452,
+          },
+          {
+            name: 'KitKat',
+            description: 518,
+          },
+        ],
+        headers: [
+            'Name', 'Description'
+        ]
     }
+  },
+  mounted () {
+    this.$refs.Tables.datas = this.data
+    this.$refs.Tables.headers = this.headers
   }  
 }
 </script>
