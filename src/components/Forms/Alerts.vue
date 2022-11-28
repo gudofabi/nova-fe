aaa137c4065d1384a0c122a1f8e745a34270314a
Alvin <ailustrisimo@yondu.com>
2022-11-28 10:21:48 +0800

    add: employee (not done yet)

---
+++ src/components/Forms/Alerts.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="relative">
+      <v-alert
+          :type="type"
+          elevation="2"
+      >{{ message }}</v-alert>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      props: {
+        message: {
+              type: [String, Number],
+              default: 'Successfully added!'
+          },
+          type: {
+              type: String,
+              default: 'success'
+          }
+      },
+  
+      methods: {
+          func_updateInput(event) {
+              this.$emit("update:message", event.target.value);
+          },
+      }
+  }
+  </script>
+  
+  <style>
+  .v-alert {
+    position: fixed;
+    right: 0;
+    top: 100px;
+    transform: translate(-50%, -50%);
+    margin: 0 auto; 
+   }
+  </style>
---
+++ src/views/EmployeeView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Employee Records</h1>
+    </div>
+    <v-row class="px-2 px-md-10 pt-5 pt-md-10">
+            
+        <v-col  cols="12" md="6">
+            <SampleForm />
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import SampleForm from '../components/Home/SampleForm.vue'
+
+export default {
+  components: { SampleForm },
+  data() {
+    return {
+    }
+  }  
+}
+</script>
+
+<style lang="scss">
+
+</style>
